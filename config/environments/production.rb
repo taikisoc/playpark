a13916d364bdf61d9e444f8a89cf5edcffbf1f8c
中根　泰希 <s14613tn@sfc.keio.ac.jp>
2015-04-11 15:17:18 +0900

    production.rb mailer認証機能localhost → htt://colorparkに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://colorpark.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address => 'smtp.gmail.com',
